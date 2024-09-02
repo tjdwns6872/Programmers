f72a35ed17c989f6cbb2de1e913545fae61a4066
tjdwns6872 <tjdwns6872@naver.com>
2024-09-02 20:14:15 +0900

    프로그래머스 - 두 수의 합

---
+++ Programmers_Explanation/src/Explanation_10.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Explanation_1
+ */
+public class Explanation_10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = solution(sc.nextInt(), sc.nextInt());
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = num1+num2;
+        return answer;
+    }
+}
