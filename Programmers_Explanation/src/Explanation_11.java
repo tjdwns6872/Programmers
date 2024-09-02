de89b06079dafb248c77de7c97d0f5f5ab244c17
tjdwns6872 <tjdwns6872@naver.com>
2024-09-02 20:18:39 +0900

    프로그래머스 - flag에 따라 다른 값 반환하기

---
+++ Programmers_Explanation/src/Explanation_11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Explanation_1
+ */
+public class Explanation_11 {
+
+    public int solution(int a, int b, boolean flag) {
+        int answer = flag ? a + b : a-b;
+        return answer;
+    }
+}
