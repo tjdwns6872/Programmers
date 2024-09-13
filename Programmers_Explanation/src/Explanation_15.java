0b830de28b699bb2009cc4265ffad85e0134e335
tjdwns6872 <tjdwns6872@naver.com>
2024-09-13 16:18:04 +0900

    이중우선순위큐

--- Programmers_Explanation/src/Explanation_15.java
+++ Programmers_Explanation/src/Explanation_15.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * 편지
+ * 이중우선순위큐
  */
 public class Explanation_15 {
 
-    public int solution(String message) {
-        return message.length()*2;
+    public static void main(String[] args) {
+        String[] operations = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        System.out.println(solution(operations)[0]);
+        System.out.println(solution(operations)[1]);
+    }
+
+    public static int[] solution(String[] operations) {
+        List<Integer> nums = new ArrayList<>();
+        for(String num : operations){
+            if(num.contains("I")){
+                nums.add(Integer.parseInt(num.substring(2, num.length())));
+            }else{
+                if(nums.size() > 0){
+                    if(num.contains("-1")){
+                        nums.remove(nums.indexOf(Collections.min(nums)));
+                    }else{
+                        nums.remove(nums.indexOf(Collections.max(nums)));
+                    }
+                }
+            }
+        }
+        if(nums.size() > 0){
+            return new int[]{Collections.max(nums), Collections.min(nums)};
+        }else{
+            return new int[]{0,0};
+        }
     }
 }
