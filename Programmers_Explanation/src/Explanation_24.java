e5809d8bb019950a86ccd1d1336f73bad31f6fbe
tjdwns6872 <tjdwns6872@naver.com>
2024-10-02 21:29:54 +0900

    프로그래머스 - 문자열을 정수로 바꾸기(새로 풀어볼 예정)

---
+++ Programmers_Explanation/src/Explanation_24.java
@@ -0,0 +1,13 @@
+/*
+문자열을 정수로 바꾸기
+*/
+public class Explanation_24 {
+
+    public static void main(String[] args) {
+        System.out.println(solution("-1234"));
+    }
+
+    public static int solution(String n) {
+        return Integer.parseInt(n);
+    }
+}
