91bedf8577eb7a30f245d79aebe4ebc3b73fc39a
wsj_6872 <wsj_6872@squarenet.co.kr>
2025-03-17 13:15:29 +0900

    프로그래머스 - 문자열의 앞의 n글자

---
+++ Programmers_Explanation/src/Explanation_55.java
@@ -0,0 +1,9 @@
+public class Explanation_55 {
+    public static void main(String[] args) {
+        solution("ProgrammerS123", 11);
+    }
+    public static String solution(String my_string, int n) {
+        String answer = my_string.substring(0, n);
+        return answer;
+    }
+}
