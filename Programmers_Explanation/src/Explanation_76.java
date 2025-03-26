0d6542a070b1edda62849778373a1fc67e104c7f
wsj_6872 <wsj_6872@squarenet.co.kr>
2025-03-26 16:43:19 +0900

    프로그래머스 - 부분 문자열인지 확인하기

---
+++ Programmers_Explanation/src/Explanation_76.java
@@ -0,0 +1,8 @@
+public class Explanation_76 {
+    public static void main(String[] args) {
+        solution("banana", "ana");
+    }
+    public static int solution(String my_string, String target) {
+        return my_string.contains(target)? 1 : 0;
+    }
+}
