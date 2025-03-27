d47d7f5502f556afa3c7bfd8720a5e3f8d0a4185
wsj_6872 <wsj_6872@squarenet.co.kr>
2025-03-27 13:58:18 +0900

    .

--- Programmers_Explanation/src/Explanation_80.java
+++ Programmers_Explanation/src/Explanation_80.java
@@ -1,3 +1,9 @@
 public class Explanation_80 {
-    
+    public static void main(String[] args) {
+        solution(123);
+    }
+    public static String solution(int n) {
+        String answer = "";
+        return n+answer;
+    }
 }
